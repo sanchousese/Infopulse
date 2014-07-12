e60bf7700fec1d8ba737f898c28232e91c90420a
sanchousese <saniasutula@gmail.com>
2014-07-12 15:59:31 +0400

    method @findFilesWithExe was added

--- src/Lectures/lecture12072014/Main.java
+++ src/Lectures/lecture12072014/Main.java
@@ -1,6 +1,8 @@
 package Lectures.lecture12072014;
 
 
+import java.io.File;
+
 /**
  * Created by Sania on 7/12/2014.
  */
@@ -15,6 +17,27 @@ public class Main {
         while (!queue.isEmpty()) {
             queue.dequeue();
         }
+
         System.out.println(queue);
+
+        findFilesWithExe("C:\\ADT\\eclipse");
+
+    }
+
+    private static void findFilesWithExe(String pathname) {
+        Queue<String> stringQueue = new Queue<String>();
+        String[] file = new File(pathname).list();
+
+        for (int i = 0; i < file.length; i++) {
+            stringQueue.enqueue(file[i]);
+        }
+
+        System.out.println(stringQueue);
+
+        while (!stringQueue.isEmpty()) {
+            String dequeue = stringQueue.dequeue();
+            if (dequeue.contains(".exe"))
+                System.out.println(dequeue);
+        }
     }
 }
