f66bf1806b1fb9af2edd6c1c053e00192ddbdf1d
sanchousese <saniasutula@gmail.com>
2014-07-12 18:16:33 +0400

    MouseAdapter was added

--- src/Lectures/lecture12072014/GUIApp/Win.java
+++ src/Lectures/lecture12072014/GUIApp/Win.java
@@ -4,8 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class Win extends JFrame implements ActionListener {
+public class Win extends JFrame {
     private JButton but = new JButton("Button");
     private Container container;
 
@@ -16,15 +18,34 @@ public class Win extends JFrame implements ActionListener {
         container.setLayout(null);
         container.add(but);
         but.setBounds(10, 50, 100, 30);
+
+        but.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                container.setBackground(Color.orange);
+            }
+        });
+
+        JButton button2 = new JButton("Button2");
+        container.add(button2);
+        button2.setBounds(but.getX(), but.getY() + but.getHeight(), but.getWidth(), but.getHeight());
+        button2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                but.setBounds(but.getX() + 10, 50, 100, 30);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                but.setBounds(10, 50, 100, 30);
+            }
+        });
     }
 
     public static void main(String[] args) {
         Win window = new Win();
         window.setVisible(true);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
     }
 }
